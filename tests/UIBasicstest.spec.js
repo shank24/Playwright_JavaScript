231e46eb85fa7d67e50bf90a0cf7d70d207ee83c
Shanky Kalra <shankkalra@gmail.com>
2024-04-24 17:29:25 -0300

    Scenario

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,8 +1,24 @@
-const {test} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test("First Test", async ({browser})=> {
-    
+test("Browser Context Test", async ({ browser }) => {
+
     const context = await browser.newContext();
     const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/")
+    console.log(await getTitle(page));
 
 });
+
+test("Page Test", async ({ page }) => {
+
+    await page.goto("https://google.com")
+    const getTitleToMatch = await getTitle(page);
+    console.log(await getTitle(page));
+    await expect(page).toHaveTitle(getTitleToMatch)
+    
+
+});
+
+async function getTitle(page) {
+    return await page.title();
+}
