686e650774228db72b7e04229cf65ae3f8668283
Shanky Kalra <shankkalra@gmail.com>
2024-04-25 17:34:17 -0300

    Text Extract

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,12 +1,16 @@
 const { test, expect } = require('@playwright/test');
 
-test("Browser Context Test", async ({ browser }) => {
+test.only("Browser Context Test", async ({ browser }) => {
 
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     console.log(await getTitle(page));
-
+    await page.locator('#username').fill("rahulshetty");
+    await page.locator("[type='password']").fill("learning");
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator("[style*='block']").textContent());
+    
 });
 
 test("Page Test", async ({ page }) => {
