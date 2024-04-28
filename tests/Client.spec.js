55b0472dbcaa5c5b8f2b4a167fb581b1783600f7
Shanky Kalra <shankkalra@gmail.com>
2024-04-28 18:36:28 -0300

    Ass

--- tests/Client.spec.js
+++ tests/Client.spec.js
@@ -7,10 +7,16 @@ test.only("Assignment Test", async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
 
+    //Sign In Locators
     const userName = page.locator("#userEmail");
     const password = page.locator("#userPassword");
     const signIn = page.locator('#login');
+    //Cart Locators
+    const products = page.locator(".card-body")
     const cardTitle = page.locator(".card-body b");
+    const productName = 'ZARA COAT 3';
+    const cartLocator = page.locator("[routerlink*='cart']");
+
 
     await getUrl(page, "https://rahulshettyacademy.com/client/");
     console.log(await getTitle(page));
@@ -21,7 +27,23 @@ test.only("Assignment Test", async ({ browser }) => {
     await page.waitForLoadState('networkidle');
 
     //console.log(await cardTitle.nth(0).textContent());
-    console.log(await cardTitle.allTextContents());
+    const titles = console.log(await cardTitle.allTextContents());
+    console.log(titles);
+    const countOfProducts = await products.count();
+
+    for (let i = 0; i < countOfProducts; i++) {
+        if(await products.nth(i).locator('b').textContent() == productName)
+        {
+            //Add To cart
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+        }        
+    }
+
+    await cartLocator.click();
+    const isPresent =  page.locator ("h3:has-text('ZARA COAT 3')").isVisible();
+    expect (isPresent).toBeTruthy();
+
 
 });
 
