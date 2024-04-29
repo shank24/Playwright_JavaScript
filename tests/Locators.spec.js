7060f2e1050974ef620dadf0c88c4afaf067ad84
Shanky Kalra <shankkalra@gmail.com>
2024-04-29 17:29:16 -0300

    Get ByRole

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -7,8 +7,11 @@ test("Locator Test", async ({ page }) => {
     await page.getByLabel("Gender").selectOption("Female");
     await page.getByPlaceholder("Password").fill("Hakuna Matata");
     await page.getByRole("button", {name: 'Submit'}).click();
-
-
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    await page.getByRole("link", {name: 'Shop'}).click();
+    //Locator Chaining
+    await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click();
+    
 });
 async function clickByLabel(page, locatorText) {
     await page.getByLabel(locatorText).click();
