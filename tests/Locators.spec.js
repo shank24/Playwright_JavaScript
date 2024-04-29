72a33931a437d964aae7c39f95e09ade122addb3
Shanky Kalra <shankkalra@gmail.com>
2024-04-29 14:27:40 -0300

    Locators

---
+++ tests/Locators.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require('@playwright/test');
+
+test("Assignment Test", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel("Check me out if you Love IceCreams!").click();
+
+});
