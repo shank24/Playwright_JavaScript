1ea23ceb192592eca11ceea660feb80cdaf2ecac
Shanky Kalra <shankkalra@gmail.com>
2024-04-29 14:32:25 -0300

    Code In Fun()

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -2,6 +2,11 @@ const { test, expect } = require('@playwright/test');
 
 test("Assignment Test", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/angularpractice/");
-    await page.getByLabel("Check me out if you Love IceCreams!").click();
+    clickByLabel(page,"Check me out if you Love IceCreams!");
+    clickByLabel(page,"Employed");
 
 });
+async function clickByLabel(page, locatorText) {
+    await page.getByLabel(locatorText).click();
+}
+
