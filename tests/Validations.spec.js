092b9418d73177c7ddfa7376ffffd950646c5f26
Shanky Kalra <shankkalra@gmail.com>
2024-04-29 21:03:17 -0300

    Back and Forth

--- tests/Validations.spec.js
+++ tests/Validations.spec.js
@@ -3,10 +3,15 @@ const { test, expect } = require('@playwright/test');
 test.only("Pop Validations Test", async ({ page }) => {
 
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+
     
-    await page.goto("https://www.google.com/");
+
+    /*await page.goto("https://www.google.com/");
     await page.goBack();
-    await page.goForward();
+    await page.goForward();*/
 
 
 
