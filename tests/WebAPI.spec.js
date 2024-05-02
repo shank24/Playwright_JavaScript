a7b553eb74839e9c6173bdbd51ed0da61c58bc8a
Shanky Kalra <shankkalra@gmail.com>
2024-05-02 17:34:27 -0300

    Create Order

--- tests/WebAPI.spec.js
+++ tests/WebAPI.spec.js
@@ -1,49 +1,27 @@
 const { test, expect, request } = require('@playwright/test');
+const {APIUtils} = require('./utils/APIUtils');
 
 const loginPayload = { userEmail: "creativestrikers@gmail.com", userPassword: "Abcd@1234" };
 const orderPayload = { orders: [{ country: "India", productOrderedId: "6581ca979fd99c85e8ee7faf" }] };
-
-let orderID;
-let token;
+let response;
 
 test.beforeAll(async () => {
 
     //Login APIs
     const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post
-        (
-            "https://rahulshettyacademy.com/api/ecom/auth/login",
-            { data: loginPayload }
-        )
-    expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson = await loginResponse.json();
-    token = loginResponseJson.token;
-    console.log(token);
-
-    //Order APIs
-    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-        {
-            data: orderPayload,
-            headers: {
-                'Authorization': token,
-                'Content-Type': 'application/json'
-            },
-        })
-        const orderResponseJson = await orderResponse.json();
-        console.log(orderResponseJson);
-        orderID = orderResponseJson.orders[0];
+    const apiObject = new APIUtils(apiContext,loginPayload);
+    response = await apiObject.createOrder(orderPayload);
+    
 });
 
-test.beforeEach(() => {
-
-});
 
 test("Place an Order API Test", async ({ page }) => {
+
 
     //Injecting token into Session
     page.addInitScript(value => {
         window.localStorage.setItem('token', value);
-    }, token);
+    }, response.token);
 
     await page.goto("https://rahulshettyacademy.com/client/");
     console.log(await page.title());
@@ -59,15 +37,15 @@ test("Place an Order API Test", async ({ page }) => {
 
     for (let index = 0; index < await rows.count(); index++) {
         const orderNumber = await rows.nth(index).locator("th").textContent();
-        if (orderID.includes(orderNumber)) {
+        if (response.orderID.includes(orderNumber)) {
             await rows.nth(index).locator("button").first().click();
             break;
         }
     }
 
     const orderIdDetail = await page.locator(".col-text").textContent();
-    await page.pause();
-    expect(orderID.includes(orderIdDetail)).toBeTruthy();
+    //await page.pause();
+    expect(response.orderID.includes(orderIdDetail)).toBeTruthy();
 
     const emailAtOrderPage = page.locator("div p:has-text(' creativestrikers@gmail.com ')");
     expect(email.includes(emailAtOrderPage));
