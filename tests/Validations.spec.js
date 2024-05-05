658b64b4098d0f64d22ed246acc9745d66e84e46
Shanky Kalra <shankkalra@gmail.com>
2024-05-04 22:45:47 -0300

    Visual Test

--- tests/Validations.spec.js
+++ tests/Validations.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only("Pop Validations Test", async ({ page }) => {
+test("Pop Validations Test", async ({ page }) => {
 
     /*await page.goto("https://www.google.com/");
     await page.goBack();
@@ -26,5 +26,29 @@ test.only("Pop Validations Test", async ({ page }) => {
 
     const subscriberNumber = await framesPage.locator(".text h2 span").textContent();
     console.log(subscriberNumber);
+
+});
+
+
+test("Screenshot & Visual Comparison Test", async ({ page }) => {
+
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    //Screenshot of specific element
+    await page.locator('#displayed-text').screenshot({path: 'partialScreenshot.png'});
+
+    await page.locator("#hide-textbox").click();
+    //Screenshot of whole page
+    //await page.screenshot({path: 'screenshot.png'});
+    await expect(page.locator("#displayed-text")).toBeHidden();
+
+});
+
+
+test.only("Image Comparison Test", async ({ page }) => {
+
+
+    await page.goto("https://www.google.com/");
+    expect (await page.screenshot()).toMatchSnapshot('landing.png');
 
 });
