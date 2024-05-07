afd59adede907bfa3fc02f5d2614d8fae8578349
Shanky Kalra <shankkalra@gmail.com>
2024-05-06 23:03:26 -0300

    Excel Completed

--- tests/excelDemo.js
+++ tests/excelDemo.js
@@ -1,78 +1,60 @@
-const ExcelJs = require('exceljs');
-const { test, expect } = require('@playwright/test');
-
-
-//Way 1 via Promise
-//Workbook
-/*const workBook = new ExcelJs.Workbook();
-//Read File
-workBook.xlsx.readFile("/Users/shankykalra/Downloads/download.xlsx").then(function () {
-    //Sheet
-    const worksheet = workBook.getWorksheet('Sheet1');
-    //Traversing Row and Cell
-    worksheet.eachRow((row, rowNumber) => {
-        row.eachCell((cell, collNumber) => {
-            console.log(cell.value);
-        });
-    });
-})*/
-
-
-//Way 2 Via async and await
-async function excelTravserse() {
-    console.log("Via Function");
-    const workBook = new ExcelJs.Workbook();
-    //Read File
-    await workBook.xlsx.readFile("/Users/shankykalra/Downloads/download.xlsx")
-    //Sheet
-    const worksheet = workBook.getWorksheet('Sheet1');
-    //Traversing Row and Cell
-    worksheet.eachRow((row, rowNumber) => {
-        row.eachCell((cell, collNumber) => {
-            console.log(cell.value);
-        });
-    });
+const ExcelJs =require('exceljs');
+ 
+async function writeExcelTest(searchText,replaceText,change,filePath)
+{
+    
+  const workbook = new ExcelJs.Workbook();
+  await workbook.xlsx.readFile(filePath);
+  const worksheet = workbook.getWorksheet('Sheet1');
+  const output= await readExcel(worksheet,searchText);
+ 
+  const cell = worksheet.getCell(output.row,output.column+change.colChange);
+  cell.value = replaceText;
+  await workbook.xlsx.writeFile(filePath);
+ 
 }
-//excelTravserse();
-
-
-//Getting Specific Cell Number and Value
-
-async function writeExcelTest(searchText, replaceText, change, filePath) {
-    const workBook = new ExcelJs.Workbook();
-    //Read File
-    await workBook.xlsx.readFile(filePath)
-    //Sheet
-    const worksheet = workBook.getWorksheet('Sheet1');
-    //Traversing Row and Cell
-    const output = await readExcel(worksheet, searchText);
-
-    //Writing Specific Cell
-    const cellToWrite = worksheet.getCell(output.row, output.col + change.colChange);
-    cellToWrite.value = replaceText;
-    await workBook.xlsx.writeFile(filePath);
-}
-
-
-async function readExcel(worksheet, searchText) {
-    let output = { row: 1, col: 1 };
-
-    worksheet.eachRow((row, rowNumber) => {
-        row.eachCell((cell, collNumber) => {
-            if (cell.value === searchText) {
-                output.row = rowNumber;
-                output.col = collNumber;
-            }
-
-        })
+ 
+ 
+async function readExcel(worksheet,searchText)
+{
+    let output = {row:-1,column:-1};
+    worksheet.eachRow((row,rowNumber) =>
+    {
+          row.eachCell((cell,colNumber) =>
+          {
+              if(cell.value === searchText)
+              {
+                  output.row=rowNumber;
+                  output.column=colNumber;
+              }
+  
+  
+          }  )
+    
     })
     return output;
 }
+//update Mango Price to 350. 
+writeExcelTest("Mango",350,{rowChange:0,colChange:2},"/Users/shankykalra/Downloads/download.xlsx");
 
+/*test('Upload download excel validation',async ({page})=>
+{
+  const textSearch = 'Mango';
+  const updateValue = '350';
+  await page.goto("https://rahulshettyacademy.com/upload-download-test/index.html");
+  const downloadPromise = page.waitForEvent('download');
+  await page.locator('#downloadButton').click();
+  page.pause();
 
-//writeExcelTest("Coconut", "350",{rowChange:0, colChange:2}, "/Users/shankykalra/Downloads/download.xlsx");
-
-test('Uplaod Download Excel Validations', async ({ page }) => {
-    await page.goto("https://rahulshettyacademy.com/upload-download-test/index.html");
-
-})
+  await downloadPromise;
+  writeExcelTest(textSearch,updateValue,{rowChange:0,colChange:2},"/Users/shankykalra/Downloads/download.xlsx");
+  await page.locator("#fileinput").click();
+  await page.locator("#fileinput").setInputFiles("/Users/shankykalra/Downloads/download.xlsx");
+  const textlocator = page.getByText(textSearch);
+  const desiredRow = await page.getByRole('row').filter({has :textlocator });
+  await expect(desiredRow.locator("#cell-4-undefined")).toContainText(updateValue);
+ 
+ 
+})*/
+ 
+ 
