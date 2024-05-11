6910e5e34af7e7d59af2daa4bb73fede6244f6bd
Shanky Kalra <shankkalra@gmail.com>
2024-05-11 09:59:32 -0300

    Page Object Done

--- pageObjects/thankYouPage.js
+++ pageObjects/thankYouPage.js
@@ -1,6 +1,29 @@
-class thankYouPage{
+const { test, expect } = require('@playwright/test');
+
+class thankYouPage {
     constructor(page) {
         this.page = page;
         this.rows = page.locator("tbody tr");
+        this.orderIdAtPage = page.locator(".col-text");
+        this.emailAtOrderPage = page.locator("div p:has-text(' creativestrikers@gmail.com ')");
+    }
+
+    async verifyOrderId(orderID) {
+
+        for (let index = 0; index < await this.rows.count(); index++) {
+            const orderNumber = await this.rows.nth(index).locator("th").textContent();
+            if (orderID.includes(orderNumber)) {
+                await this.rows.nth(index).locator("button").first().click();
+                break;
+            }
+        }
+    }
+
+    async verifyOrderAndEmail(orderID, email) {
+        const orderIdDetail = await this.orderIdAtPage.textContent();
+        expect(await orderID.includes(orderIdDetail)).toBeTruthy();
+        expect(await email.includes(this.emailAtOrderPage));
     }
 }
+
+module.exports = { thankYouPage };
--- tests/ClientPO.spec.js
+++ tests/ClientPO.spec.js
@@ -3,21 +3,17 @@ const { loginPage } = require('../pageObjects/loginPage');
 const { dashBoardPage } = require('../pageObjects/dashBoardPage');
 const { checkoutPage } = require('../pageObjects/checkoutPage');
 const { orderPage } = require('../pageObjects/orderPage');
-
+const { thankYouPage } = require('../pageObjects/thankYouPage');
 
 test.only("Assignment Test", async ({ browser }) => {
 
     const context = await browser.newContext();
     const page = await context.newPage();
 
-    //Sign In Values
+    //Assertion Values
     const email = "creativestrikers@gmail.com";
     const password = "Abcd@1234";
-
-    //Cart Variable
     const productName = 'ZARA COAT 3';
-
-    //CheckOut Variable
     const countryName = "Can";
 
     //Login Page Object
@@ -43,25 +39,10 @@ test.only("Assignment Test", async ({ browser }) => {
     const orderID = await orderPageObj.getOrderId();
     await orderPageObj.clickOnOrders();
 
-    //Thank You Page
-    const rows = page.locator("tbody tr");
-    console.log(rows.count());
-
-
-    for (let index = 0; index < await rows.count(); index++) {
-        const orderNumber = await rows.nth(index).locator("th").textContent();
-        if (orderID.includes(orderNumber)) {
-            await rows.nth(index).locator("button").first().click();
-            break;
-        }
-    }
-
-    const orderIdDetail = await page.locator(".col-text").textContent();
-    expect(orderID.includes(orderIdDetail)).toBeTruthy();
-
-    const emailAtOrderPage = page.locator("div p:has-text(' creativestrikers@gmail.com ')");
-    expect(email.includes(emailAtOrderPage));
-
+    //Thank You Page Object
+    const thankYouPageObj = new thankYouPage(page);
+    await thankYouPageObj.verifyOrderId(orderID);
+    await thankYouPageObj.verifyOrderAndEmail(orderID, email);
 
 });
 
