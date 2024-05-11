4e7357ef002ccbafa57d3cb282a85bd6ecdd0c22
Shanky Kalra <shankkalra@gmail.com>
2024-05-11 20:34:51 -0300

    Add Json For Data

--- tests/ClientPO.spec.js
+++ tests/ClientPO.spec.js
@@ -1,36 +1,34 @@
 const { test, expect } = require('@playwright/test');
 const {POManager} = require('../pageObjects/POManager');
 
+//Converting JSON Object -> String Object -> JavaScript Object
+const testData = JSON.parse(JSON.stringify(require('../utils/placeOrderTestData.json')));
+
 
 test.only("Assignment Test", async ({ page }) => {
 
+    //Page Object Manager Object Instantiation
     const POManagerObj = new POManager(page);
-
-    //Assertion Values
-    const email = "creativestrikers@gmail.com";
-    const password = "Abcd@1234";
-    const productName = 'ZARA COAT 3';
-    const countryName = "Can";
 
     //Login Page Object
     const loginPageObj = POManagerObj.getLoginPage();
     await loginPageObj.goTo();
-    await loginPageObj.validLogin(email, password);
+    await loginPageObj.validLogin(testData.email, testData.password);
     await loginPageObj.getTitleOfPage(page);
 
     //Dashboard Page Object
     const dashPageObj = POManagerObj.getDashboardPage();
-    await dashPageObj.searchProduct(productName);
+    await dashPageObj.searchProduct(testData.productName);
     await dashPageObj.navigateToCart();
 
     //Checkout Page Object
     const checkoutPageObj = POManagerObj.getCheckoutPage();
-    await checkoutPageObj.clickUntilCheckOutBtn(productName);
-    await checkoutPageObj.selectCountry(countryName);
+    await checkoutPageObj.clickUntilCheckOutBtn(testData.productName);
+    await checkoutPageObj.selectCountry(testData.countryName);
 
     //Order Page Object
     const orderPageObj = POManagerObj.getOrderPage();
-    await orderPageObj.verifyEmail(email);
+    await orderPageObj.verifyEmail(testData.email);
     await orderPageObj.verifyOrderConfirmation();
     const orderID = await orderPageObj.getOrderId();
     await orderPageObj.clickOnOrders();
@@ -38,7 +36,7 @@ test.only("Assignment Test", async ({ page }) => {
     //Thank You Page Object
     const thankYouPageObj = POManagerObj.getThankyouPage();
     await thankYouPageObj.verifyOrderId(orderID);
-    await thankYouPageObj.verifyOrderAndEmail(orderID, email);
+    await thankYouPageObj.verifyOrderAndEmail(orderID, testData.email);
 
 });
 
