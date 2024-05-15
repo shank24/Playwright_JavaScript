8c8503de08334fad3728ff144dd1d97ccc902aa7
Shanky Kalra <shankkalra@gmail.com>
2024-05-15 20:58:45 -0300

    Step Def

--- features/Ecommerce.feature
+++ features/Ecommerce.feature
@@ -1,9 +1,9 @@
 Feature: Ecommerce Validation
 
     Scenario: Placing The Order
-        Given When I login to the Ecommerce application with "username" and "password"
+        Given When I login to the Ecommerce application with "anshika@gmail.com" and "Iamking@000"
         When I add product "ZARA COAT 3"
-        Then I verify "ZARA COAT 3" is displayed succesfully
-        When Enter valid details and placed the order
-        Then I verify the order is present in the order history
+        Then I verify "ZARA COAT 3" is displayed succesfully for "Canada"
+        When Enter valid details "anshika@gmail.com" and placed the order
+        Then I verify the order is present in the order history for "anshika@gmail.com"
         
--- features/stepDefinitions/steps.js
+++ features/stepDefinitions/steps.js
@@ -1,52 +1,53 @@
 const { When, Then, Given } = require('@cucumber/cucumber');
 const { POManager } = require('../../pageObjects/POManager');
-const { test, expect, playwright } = require('@playwright/test');
+const playwright = require('@playwright/test');
 
+let orderID;
+Given('When I login to the Ecommerce application with {string} and {string}', { timeout: 100 * 1000 }, async function (username, password) {
 
-Given('When I login to the Ecommerce application with {string} and {string}', async function (string, string2) {
-
-    const browser = await playwright.chromium.launch();
-
+    const browser = await playwright.chromium.launch({
+        headless:false
+    });
     const context = await browser.newContext();
     const page = await context.newPage();
 
     //Page Object Manager Object Instantiation
-    const POManagerObj = new POManager(page);
+    this.POManagerObj = new POManager(page);
 
     //Login Page Object
-    const loginPageObj = POManagerObj.getLoginPage();
+    const loginPageObj = this.POManagerObj.getLoginPage();
     await loginPageObj.goTo();
-    await loginPageObj.validLogin(data.email, data.password);
+    await loginPageObj.validLogin(username, password);
     await loginPageObj.getTitleOfPage(page);
 
 });
 
-When('I add product {string}', async function (string) {
+When('I add product {string}', async function (productName) {
     //Dashboard Page Object
-    const dashPageObj = POManagerObj.getDashboardPage();
-    await dashPageObj.searchProduct(data.productName);
+    const dashPageObj = this.POManagerObj.getDashboardPage();
+    await dashPageObj.searchProduct(productName);
     await dashPageObj.navigateToCart();
 });
 
-Then('I verify {string} is displayed succesfully', async function (string) {
-    const checkoutPageObj = POManagerObj.getCheckoutPage();
-        await checkoutPageObj.clickUntilCheckOutBtn(data.productName);
-        await checkoutPageObj.selectCountry(data.countryName);
+Then('I verify {string} is displayed succesfully for {string}', async function (productName, countryName) {
+    const checkoutPageObj = this.POManagerObj.getCheckoutPage();
+    await checkoutPageObj.clickUntilCheckOutBtn(productName);
+    await checkoutPageObj.selectCountry(countryName);
 });
 
-When('Enter valid details and placed the order', async function () {
+When('Enter valid details {string} and placed the order', async function (email) {
     //Order Page Object
-    const orderPageObj = POManagerObj.getOrderPage();
-    await orderPageObj.verifyEmail(data.email);
+    const orderPageObj = this.POManagerObj.getOrderPage();
+    await orderPageObj.verifyEmail(email);
     await orderPageObj.verifyOrderConfirmation();
-    const orderID = await orderPageObj.getOrderId();
+    orderID = await orderPageObj.getOrderId();
     await orderPageObj.clickOnOrders();
 });
 
-Then('I verify the order is present in the order history', async function () {
+Then('I verify the order is present in the order history for {string}', async function (email) {
     //Thank You Page Object
-    const thankYouPageObj = POManagerObj.getThankyouPage();
+    const thankYouPageObj = this.POManagerObj.getThankyouPage();
     await thankYouPageObj.verifyOrderId(orderID);
-    await thankYouPageObj.verifyOrderAndEmail(orderID, data.email);
+    await thankYouPageObj.verifyOrderAndEmail(orderID, email);
 });
 
